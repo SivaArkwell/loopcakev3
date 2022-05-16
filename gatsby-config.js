8763cec47c484123296075bd13c83a0179f850c1
Andrew Arkwell <andrew@sivacreative.com>
2022-05-15 22:55:04 -0400

    Changing branding

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `LoopCake`,
+    description: `LoopCake - Web Applications | Mobile Apps | Web Development | Analytical SEO | `,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
